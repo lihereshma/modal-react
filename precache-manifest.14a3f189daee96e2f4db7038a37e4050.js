af6dabf1c81002fd1a491f3a4ad41ecba87816fc
lihereshma <reshmalihe00068@gmail.com>
2020-09-11 16:40:07 +0530

    Updates

---
+++ precache-manifest.14a3f189daee96e2f4db7038a37e4050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a884e423f82e357cd9933b07e308e259",
+    "url": "/modal-react/index.html"
+  },
+  {
+    "revision": "2a6ce02a335d36220d80",
+    "url": "/modal-react/static/css/main.1c4325d2.chunk.css"
+  },
+  {
+    "revision": "25cdf52c940a0b044d0c",
+    "url": "/modal-react/static/js/2.25ede715.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/modal-react/static/js/2.25ede715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6ce02a335d36220d80",
+    "url": "/modal-react/static/js/main.dcd69ac1.chunk.js"
+  },
+  {
+    "revision": "bcf5096a556eb6753a94",
+    "url": "/modal-react/static/js/runtime-main.f6db189c.js"
+  }
+]);
