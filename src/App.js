e73c27c64d3c6d0742e431a9bf7ca8fef0719cad
lihereshma <reshmalihe00068@gmail.com>
2020-09-11 16:41:01 +0530

    project added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Modal from 'react-modal'
+import { FaWindowClose } from "react-icons/fa";
+
+Modal.setAppElement('#root')
 
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="open-button" onClick = { () => setModalIsOpen(true) }>Open Modal</button>
+      <Modal
+        isOpen = { modalIsOpen } 
+        shouldCloseOnOverlayClick = { false }
+        onRequestClose = { () => setModalIsOpen(false) }
+        style = {
+          {
+            overlay: {
+              backgroundColor : 'rgba(0, 0, 0, 0.7)'
+            },
+            content: {
+            
+            width: '400px',
+            top: '50%',
+            left: '50%',          
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#fff',
+            }
+          }
+        }   
+      >
+        <FaWindowClose className="close-button" onClick = { () => setModalIsOpen(false) }/>
+        <div className="modal-content">
+          <h2>Welcome here...</h2>
+          <p>
+            Woohoo, you're reading this text in a modal!
+          </p>
+          <span>Press Close button or Esc button to close model</span>
+        </div>
+      </Modal>
     </div>
   );
 }
